160d940302040a3acc13c2fe16ad38d35ca04a13
BMasquio <brunomasquio@gmail.com>
2020-02-03 12:18:03 -0300

    Issue #491 - Cache busting for js and css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,18 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
 
-let LiveReloadPlugin = require('webpack-livereload-plugin');
+let LiveReloadPlugin = require('webpack-livereload-plugin')
 
-mix.js(['resources/assets/js/app.js', 'resources/assets/packages/select2-to-tree/select2-to-tree-master/src/select2totree.js'], 'public/js/app.js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+  .js(
+    [
+      'resources/assets/js/app.js',
+      'resources/assets/packages/select2-to-tree/select2-to-tree-master/src/select2totree.js',
+    ],
+    'public/js/app.js',
+  )
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  .version()
 
 mix.webpackConfig({
-    plugins: [
-        new LiveReloadPlugin(),
-    ]
-});
+  plugins: [new LiveReloadPlugin()],
+})
