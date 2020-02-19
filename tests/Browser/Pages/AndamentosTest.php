d5238b1b87caa36487832ae465a03af4e89567bb
ffmatheus <matheusbfrfg@hotmail.com>
2020-02-19 15:03:12 +0000

    andamentos test ajustes

--- tests/Browser/Pages/AndamentosTest.php
+++ tests/Browser/Pages/AndamentosTest.php
@@ -128,8 +128,8 @@ class AndamentosTest extends DuskTestCase
         $tipoPrazoA = app(TiposPrazosRepository::class)
             ->randomElement()
             ->toArray();
-        $dataPrazoA = \DateTime::createFromFormat('d-m-Y', '03-02-2333');
-        $dataEntregaA = \DateTime::createFromFormat('d-m-Y', '04-05-2444');
+        $dataPrazoA = \DateTime::createFromFormat('m-d-Y', '03-02-2333');
+        $dataEntregaA = \DateTime::createFromFormat('m-d-Y', '04-05-2444');
         $observacaoA = only_letters_and_space($faker->name);
 
         $numProcesso = static::$processoAndamento['numero_judicial'];
@@ -143,6 +143,7 @@ class AndamentosTest extends DuskTestCase
             $observacaoA,
             $numProcesso
         ) {
+            app()->setlocale('em');
             $browser
                 ->visit('/andamentos')
                 ->clickLink($numProcesso)
