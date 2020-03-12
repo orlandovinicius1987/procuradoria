cbd0c6a3603dfe7670dcbb383c8170e50f3bdf2c
BMasquio <brunomasquio@gmail.com>
2020-03-12 13:45:42 -0300

    Issue #482

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">Ocorreu um erro grave, os administradores do sistema foram avisados</h1>
+    <h1 class="text-center">Ocorreu um erro interno no sistema, os administradores do sistema foram avisados</h1>
 @stop
